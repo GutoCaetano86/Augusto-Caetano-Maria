5d2c3f5f8f6801f38184e0333f0dacdb67d71594
Augusto Caetano Maria <augusto.maria@yara.com>
2022-05-03 01:36:25 -0300

    paginaatualizada

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,37 @@
 function Home() {
-    return <div>Home</div>
+  return (
+        <body>
+            <div class="container">
+                <header>
+                    <div class="center">
+                        <h1>Minhas redes sociais</h1>
+                        <h2>Quem sou eu?</h2>
+                    </div>
+                </header>
+                <section id="quemsou">
+                <div id="quemsoueu">
+                        <p>Meu nome é Augusto Caetano Maria, sou Analista Tributário e KeyUser SAP e Mastersaf, 
+                            responsável pela integridade entre banco de dados SAP e MSAF.
+                            Sou uma pessoa determinada e dedicada, que aposta em desafios, criatividade e motivação em torno de objetivos. 
+                            Sempre em busca de qualificação, conhecimento e aperfeiçoamento na área tributária e tecnológica.
+                            Foco nos estudos e busca constante por informação, qualificação e aperfeiçoamento.
+                        </p>
+                        <p>Sou um entusiasta em aprender linguagens de programação, criação de sites, aplicativos e programas.</p>
+                    </div>
+                </section>
+                <section id="redes">
+                    <div id="redessociais">
+                        <h2>Como falar comigo?</h2>
+                        <ul>
+                        
+                        </ul>
+                    </div>
+                </section>
+                <footer>
+                    <p>&copy; Augusto Caetano Maria</p>
+                </footer>
+            </div>
+        </body>
+    )
 }
-export default Home
+export default  Home
