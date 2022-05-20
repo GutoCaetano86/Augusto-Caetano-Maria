9367ab57adc3f4ae242760c5af1726a91272e322
Augusto Caetano Maria <augusto.maria@yara.com>
2022-05-19 23:44:24 -0300

    classe

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,8 @@ import Menu from '../components/Menu'
 
 
 export async function getServerSideProps() {
-    const response = await fetch(`http://localhost:3000/api/app`);
-    //const response = await fetch('/api/app');
-    //const response = await axios.get('/api/app')
+    const response = await fetch(`http://localhost:8080`);
     const data = await response.json();
-    //console.log(data);
 
     return { props: { data } };
 }
